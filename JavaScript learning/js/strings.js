1bd40c5dddfa1768394a0194515c4c4301732c11
Jose Nicolas Melchor Gutierrez <jnmelchorguti@gmail.com>
2024-03-13 15:04:49 +0000

    This part covers the basics of strings with some string functions in JavaScript

---
+++ JavaScript learning/js/strings.js
@@ -0,0 +1,19 @@
+const myVariable = "Mathematics";
+
+console.log(myVariable.length)
+
+console.log(myVariable.charAt(5))
+
+console.log(myVariable.indexOf("mat"))
+
+console.log(myVariable.lastIndexOf("ath"))
+
+console.log(myVariable.slice(6, 8))
+
+console.log(myVariable.toUpperCase())
+
+console.log(myVariable.toLowerCase())
+
+console.log(myVariable.includes("div"))
+
+console.log(myVariable.split("e"))
