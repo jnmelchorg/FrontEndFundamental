98c917c6273432b600bdb1a4462bd3caca022f14
Jose Nicolas Melchor Gutierrez <jnmelchorguti@gmail.com>
2024-03-13 21:11:13 +0000

    This part covers the basics of the math object in JavaScript and some of its methods

---
+++ JavaScript learning/js/math_methods.js
@@ -0,0 +1,19 @@
+console.log(Math.PI);
+
+console.log(Math.trunc(Math.PI));
+
+console.log(Math.round(Math.PI));
+
+console.log(Math.ceil(Math.PI));
+
+console.log(Math.floor(Math.PI));
+
+console.log(Math.pow(5, 2));
+
+console.log(Math.min(5, 0.5, 32, 24));
+
+console.log(Math.max(5, 2, 32, 24));
+
+console.log(Math.random());
+
+console.log(Math.floor(Math.random() * 10) + 1);
